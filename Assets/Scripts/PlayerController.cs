59d8ae85c9caa784f1af937518a7f09581db8fe9
Diptark Bose <diptark.bose@gmail.com>
2022-02-16 12:01:59 -0500

    Added Key Piece Collectibles

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,36 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
-    //public Rigidbody rb;
     public float jumpForce;
     public float gravityScale;
     public CharacterController characterController;
-
     private Vector3 moveDirection;
-    
+
+    private int keyPieceCount;
+    public TextMeshProUGUI keyPieceCountText;
+
     // Start is called before the first frame update
     void Start()
     {
-        //rb = GetComponent<Rigidbody>();
         characterController = GetComponent<CharacterController>();
+        keyPieceCount = 0;
+        setKeyPieceCountText();
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*
-        rb.velocity = new Vector3(Input.GetAxis("Horizontal")*moveSpeed, rb.velocity.y, Input.GetAxis("Vertical") * moveSpeed);
-        if(Input.GetButtonDown("Jump"))
-        {
-            rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
-        }
-        */
-        //moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, moveDirection.y, Input.GetAxis("Vertical") * moveSpeed);
-        
         float yStore = moveDirection.y;
         moveDirection = (transform.forward * Input.GetAxis("Vertical")) + (transform.right * Input.GetAxis("Horizontal"));
         moveDirection = moveDirection.normalized * moveSpeed;
@@ -46,5 +40,20 @@ public class PlayerController : MonoBehaviour
         }
         moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale * Time.deltaTime);
         characterController.Move(moveDirection * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Collectible"))
+        {
+            other.gameObject.SetActive(false);
+            keyPieceCount++;
+            setKeyPieceCountText();
+        }
+    }
+
+    void setKeyPieceCountText()
+    {
+        keyPieceCountText.text = "Key Pieces: "+keyPieceCount+"/3";
     }
 }

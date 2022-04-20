9323ca5f9c7a6439ed054f28599f32d17d353275
Diptark Bose <diptark.bose@gmail.com>
2022-04-20 19:36:41 -0400

    Added Animation for Lava Floor

--- Assets/Scripts/LavaFloorScript.cs
+++ Assets/Scripts/LavaFloorScript.cs
@@ -6,6 +6,8 @@ public class LavaFloorScript : MonoBehaviour
 {
     // Start is called before the first frame update
     AudioSource adSrc;
+    bool manInPit = false;
+    Animator playerAnimator;
     void Start()
     {
         adSrc = GetComponent<AudioSource>();
@@ -14,15 +16,27 @@ public class LavaFloorScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if (manInPit)
+        {
+            if (!AnimatorIsPlaying())
+                playerAnimator.Play("Jumping");
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         adSrc.Play();
+        playerAnimator = other.GetComponent<Animator>();
+        manInPit = true;
     }
     private void OnTriggerExit(Collider other)
     {
         adSrc.Stop();
+        manInPit = false;
+    }
+    bool AnimatorIsPlaying()
+    {
+        return playerAnimator.GetCurrentAnimatorStateInfo(0).length >
+               playerAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime;
     }
 }

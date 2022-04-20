c8808084094a1d5a24946d2cdee555ba84f6c87b
Diptark Bose <diptark.bose@gmail.com>
2022-04-20 13:24:27 -0400

    Developed and Configured Lava Floor Prefab

---
+++ Assets/Scripts/FireChamberAudio.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireChamberAudio : MonoBehaviour
+{
+    // Start is called before the first frame update
+    AudioSource adSrc;
+    void Start()
+    {
+        adSrc = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        adSrc.Play();
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        adSrc.Stop();
+    }
+}
